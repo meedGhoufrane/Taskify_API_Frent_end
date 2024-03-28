680b6715467ca8a2e0af18230b96f225208ef082
meed ghoufrane <baloncesmeed@gmail.com>
2024-03-28 11:18:04 +0000

    add repo

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+// import { createPinia } from 'pinia'
+
+import router from './router'
+// import './axios'
+import App from './App.vue'
+import './index.css'
+
+const pinia = createPinia()
+const app = createApp(App)
+app.use(pinia)
+app.use(router)
+
+app.mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import Home from '../components/Home.vue'
+
+const routes = [
+    { path: '/', name: 'Home', component: Home },
+    { path: '/login', name: 'Login', component: ()=>import('../components/Login.vue') },
+    { path: '/register', name: 'Register', component: ()=>import('../components/Register.vue') },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
