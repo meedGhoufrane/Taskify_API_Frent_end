4584f15f5d0a2bd6c0bb77bf206b1a0fb36e5468
mohammed Ghoufrane <93007768+meedGhoufrane@users.noreply.github.com>
2024-03-31 13:44:12 +0000

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,7 +4,7 @@ export default {};
 <template>
   <header>
     <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-      <div
+      <div 
         class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl"
       >
         <router-link :to="{name: 'Home'}" class="flex items-center">
